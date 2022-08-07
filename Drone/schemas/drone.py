1f6c43df8a048db8520ad19f49abe2de52ba8070
Yuniel <yuni.boss@gmail.com>
2022-08-07 12:52:17 -0400

    schemas pydantic drone and medication.py

---
+++ Drone/schemas/drone.py
@@ -0,0 +1,52 @@
+from enum import Enum
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+from schemas.medication import Medication
+
+
+class ModelDrone(str, Enum):
+    lightweight = 'Lightweight'
+    middleweight = 'Middleweight'
+    cruiserweight = 'Cruiserweight'
+    heavyweight = 'Heavyweight'
+
+
+class State(str, Enum):
+    idle = 'IDLE'
+    loading = 'LOADING'
+    loaded = 'LOADED'
+    delivering = 'DELIVERING'
+    delivered = 'DELIVERED'
+    returning = 'RETURNING'
+
+
+class Drone(BaseModel):
+    id: int
+    serial_number: str = Field(
+        min_length=0,
+        max_length=100
+    )
+    model: ModelDrone
+    weight_limit: float = Field(
+        exclusiveMaximum=500
+    )
+    battery_capacity: float
+    state: State
+    medications: list[Medication] = []
+
+    class Config:
+        orm_mode = True
+
+
+class DroneUpdate(BaseModel):
+    serial_number: str
+
+
+class DroneGet(BaseModel):
+    serial_number: str
+
+
+class Response(BaseModel):
+    message: str
---
+++ Drone/schemas/medication.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class Medication(BaseModel):
+    id: int
+    name: str
+    weight: int
+    code: str
+    image: Optional[str]
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class MedicationUpdate(BaseModel):
+    name: str
+
+
+class MedicationGet(BaseModel):
+    name: str
+
+
+class Response(BaseModel):
+    message: str
