761e6c22fa26ad9f92c787cdab7aadf102b95ba3
YuniGoldenBoY30 <48262217+YuniGoldenBoY30@users.noreply.github.com>
2022-08-09 09:31:00 -0400

    update add validator

--- Drone/schemas/medication.py
+++ Drone/schemas/medication.py
@@ -1,6 +1,10 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator, PydanticValueError
+
+
+class NotValidEntry(PydanticValueError):
+    msg_template = 'not valid entry, got "{wrong_value}"'
 
 
 class Medication(BaseModel):
@@ -10,9 +14,36 @@ class Medication(BaseModel):
     code: str
     image: Optional[str]
     drone_id: int
-
+    
     class Config:
         orm_mode = True
+    
+    @validator('name')
+    def valid_name(cls, name):
+        name = str(name).strip().lower()
+        
+        for letter in name:
+            criteria = (
+                letter.isalnum(),
+                letter == "-",
+                letter == "_"
+            )
+            if not any(criteria):
+                raise NotValidEntry(wrong_value = name)
+        return name
+    
+    @validator('code')
+    def valid_code(cls, code):
+        code = str(code).strip().upper()
+        
+        for letter in code:
+            criteria = (
+                letter.isalnum(),
+                letter == "_",
+            )
+            if not any(criteria):
+                raise NotValidEntry(wrong_value = code)
+        return code
 
 
 class MedicationUpdate(BaseModel):
